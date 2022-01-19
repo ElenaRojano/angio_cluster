a7e5af45537dde000abfbd9d49a198e1251658e5
seoanezonjic <seoanezonjic@hotmail.com>
2022-01-19 12:52:42 +0100

    Minor updates

--- executions/get_from_onto.sh
+++ executions/get_from_onto.sh
@@ -34,7 +34,7 @@ if [ "$1" == "2" ]; then
 fi
 
 if [ "$1" == "3" ]; then
-	go=$2
+	go=$2 #GO:0001525 => Angiogenesis
 	cut -f 2,5 $temp_files'/gene_disease.9606.tsv' > $temp_files'/gene_MONDO4go.txt'
 	grep -w $go $temp_files'/goa_human.gaf' | awk '{if($7 != "IEA" && $7 != "NAS" && $7 != "ISS") print $3}' | sort -u > $list_path/go_gene.lst
 	grep -w -F -f $list_path/go_gene.lst $temp_files'/gene_MONDO4go.txt' > $list_path'/gene_MONDO4go_filt.txt'
